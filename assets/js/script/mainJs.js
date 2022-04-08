3e3b6cc1adabb0a23eea35cede280f9282811e3c
DLP-Sekara <dslahirupiyumal99@gmail.com>
2022-04-08 21:16:25 +0530

    target box created

--- assets/js/script/mainJs.js
+++ assets/js/script/mainJs.js
@@ -1,3 +1,21 @@
 for (var i=0;i<132;i++){
     $("#grid").append("<div></div>");
 }
+var targetBox=[
+    0,1,2,3
+];
+var pathBox=[
+    0,1,2,3
+];
+var bombBox=[
+    0,1,2,3
+];
+const allDiv = Array.from($("#grid div"));
+
+setTarget();
+function setTarget() {
+    for (var i=0;i<targetBox.length;i++){
+        allDiv[targetBox[i]].classList.add("target");
+    }
+}
+
