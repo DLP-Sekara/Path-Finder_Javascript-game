5ce550485ce0cb525b96feca1306ce22b9b29287
DLP-Sekara <dslahirupiyumal99@gmail.com>
2022-04-08 23:01:28 +0530

    added bomb path

--- assets/js/script/mainJs.js
+++ assets/js/script/mainJs.js
@@ -8,7 +8,7 @@ var pathBox=[
     0,1,2,3
 ];
 var bombBox=[
-    0,1,2,3
+    45,50,55,58,59,63,64,68,71,73,75,77,79,82,83,86,89,92,95,97,105,112,114,118,122,127,130
 ];
 const allDiv = Array.from($("#grid div"));
 
@@ -18,4 +18,9 @@ function setTarget() {
         allDiv[targetBox[i]].classList.add("target");
     }
 }
-
+setBomb();
+function setBomb() {
+    for (var i=0;i<bombBox.length;i++){
+        allDiv[bombBox[i]].classList.add("bomb");
+    }
+}
