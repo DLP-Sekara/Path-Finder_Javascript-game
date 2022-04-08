7e663a5f8cfce57653b058ed67d2b21bde28d7fa
DLP-Sekara <dslahirupiyumal99@gmail.com>
2022-04-08 19:07:05 +0530

    grid layout

--- assets/js/script/mainJs.js
+++ assets/js/script/mainJs.js
@@ -1,3 +1,3 @@
-//
- //$("#main").append("<div style='height: 100px;width: 100px;background-color: blue;z-index: 1'>"+"lorem"+"</div>");
- //$("#main").append("<p style='color: white'>"+"lorem helo"+"</p>");
+for (var i=0;i<132;i++){
+    $("#grid").append("<div></div>");
+}
