1454f1f2d6446d4c357615d36ce4d0985138fac5
DLP-Sekara <dslahirupiyumal99@gmail.com>
2022-04-08 18:25:04 +0530

    JS Game_Path Finder

---
+++ assets/js/script/mainJs.js
@@ -0,0 +1,3 @@
+//
+ //$("#main").append("<div style='height: 100px;width: 100px;background-color: blue;z-index: 1'>"+"lorem"+"</div>");
+ //$("#main").append("<p style='color: white'>"+"lorem helo"+"</p>");
