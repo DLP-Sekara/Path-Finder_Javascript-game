a9df777956a55b673fb91756e191d6c325ffc24b
DLP-Sekara <dslahirupiyumal99@gmail.com>
2022-04-09 11:36:01 +0530

    target and path

--- assets/js/script/mainJs.js
+++ assets/js/script/mainJs.js
@@ -5,7 +5,8 @@ var targetBox=[
     0,1,2,3
 ];
 var pathBox=[
-    0,1,2,3
+    44,46,47,48,49,51,52,53,54,56,57,60,61,62,65,66,67,69,70,72,74,76,78,80,81,84,85,87,88,90,91,93,94,96,98,99,100,101,
+    102,103,104,106,107,108,109,110,111,113,115,116,117,119,120,121,123,124,125,126,128,129,131
 ];
 var bombBox=[
     45,50,55,58,59,63,64,68,71,73,75,77,79,82,83,86,89,92,95,97,105,112,114,118,122,127,130
@@ -18,9 +19,16 @@ function setTarget() {
         allDiv[targetBox[i]].classList.add("target");
     }
 }
+
 setBomb();
 function setBomb() {
     for (var i=0;i<bombBox.length;i++){
         allDiv[bombBox[i]].classList.add("bomb");
+    }
+}
+setPath();
+function setPath() {
+    for (var i=0;i<pathBox.length;i++){
+        allDiv[pathBox[i]].classList.add("path");
     }
 }
